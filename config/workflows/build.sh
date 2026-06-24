db2c5e914195b8dd62fa655264c2fa6c3432d65a
Ratha Heang <ratha@web-essentials.co>
2026-06-24 16:05:48 +0700

    fix ansible

--- config/workflows/build.sh
+++ config/workflows/build.sh
@@ -3,7 +3,7 @@
 FILE_NAME=$(basename "$0")
 STAGE="${FILE_NAME%.*}"
 
-ansible-playbook config/ansible/playbook.yml -vv \
+ansible-playbook config/ansible/playbook.yaml -vv \
       -t build \
       -e version=${BUILD_ID} \
       -e workspace=${WORKSPACE}
